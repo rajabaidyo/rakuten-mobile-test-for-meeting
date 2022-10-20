96015099d81dc99f19548b5418604be8490ef519
unknown <kennykarnama@gmail.com>
2022-10-20 17:58:24 +0700

    add liquibase migration

--- apps/user-api/src/main/java/com/rakutenmobile/userapi/user/adapter/in/restful/reactive/UserController.java
+++ apps/user-api/src/main/java/com/rakutenmobile/userapi/user/adapter/in/restful/reactive/UserController.java
@@ -4,12 +4,14 @@ import com.rakutenmobile.openapi.models.RegisterRequest;
 import com.rakutenmobile.openapi.models.User;
 import com.rakutenmobile.openapi.spring.reactive.api.RegisterApi;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
-
+@RestController
 public class UserController implements RegisterApi {
     @Override
     public Mono<ResponseEntity<User>> registerPost(Mono<RegisterRequest> registerRequest, ServerWebExchange exchange) {
-        return null;
+        return Mono.empty();
     }
 }
