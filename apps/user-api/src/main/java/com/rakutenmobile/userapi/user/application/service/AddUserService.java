5d5ca8506fbbfeb52b744e5ee9ce9e2686341ae8
unknown <kennykarnama@gmail.com>
2022-10-20 21:11:28 +0700

    add bcrypt

--- apps/user-api/src/main/java/com/rakutenmobile/userapi/user/application/service/AddUserService.java
+++ apps/user-api/src/main/java/com/rakutenmobile/userapi/user/application/service/AddUserService.java
@@ -4,7 +4,9 @@ import com.rakutenmobile.userapi.user.adapter.out.persistence.UserEntity;
 import com.rakutenmobile.userapi.user.adapter.out.persistence.UserRepository;
 import com.rakutenmobile.userapi.user.application.port.in.AddUserUseCase;
 import com.rakutenmobile.userapi.user.domain.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import reactor.core.publisher.Mono;
 
 import java.util.UUID;
@@ -13,13 +15,18 @@ public class AddUserService implements AddUserUseCase {
 
     private final UserRepository userRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public AddUserService(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
     @Override
     public Mono<User> AddSingleUser(String name, String password) {
-        UserEntity newUser = UserEntity.builder().name(name).password(password).build();
+        UserEntity newUser = UserEntity.builder().name(name)
+                .password(passwordEncoder.encode(password))
+                .build();
         Mono<UserEntity> saved = userRepository.save(newUser);
         return saved.map(userEntity -> User.builder().userId(userEntity.getUserId())
                 .name(userEntity.getName()).password(userEntity.getPassword())
--- apps/user-api/src/main/java/com/rakutenmobile/userapi/user/configuration/BeanConfiguration.java
+++ apps/user-api/src/main/java/com/rakutenmobile/userapi/user/configuration/BeanConfiguration.java
@@ -7,6 +7,8 @@ import com.rakutenmobile.userapi.user.application.service.AddUserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @ComponentScan(basePackageClasses = UserApiApplication.class)
@@ -15,5 +17,10 @@ public class BeanConfiguration {
     @Bean
     AddUserUseCase customerService(final UserRepository userRepository) {
         return new AddUserService(userRepository);
+    }
+
+    @Bean
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
     }
 }
